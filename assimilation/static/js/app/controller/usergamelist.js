9a9ddbcbb1bedeef0f1cbcfe06be949ed0a9656c
Ryan <stringham@gmail.com>
2012-11-14 11:33:35 -0700

    updated usergamelist client to display games that are still initiating

--- assimilation/static/js/app/controller/usergamelist.js
+++ assimilation/static/js/app/controller/usergamelist.js
@@ -33,47 +33,59 @@ assimilation.UserGameList.prototype.addGame = function(game) {
 		this.games[game['id']].remove();
 	}
 	var newGameContainer = $("<div>").addClass('game-list-item');
-	var left = $('<div>').addClass('game-item-left');
-	var players = $('<div>').addClass('players').append($('<div>').addClass('players-title').text('Players'));	
-	var list = $('<ul>');
-	for(var i=0; i<game.users.length; i++){
-		var user = game.users[i];
-		var li = $('<li>').addClass(user['color']);
-		if(user['id'] == game['currentPlayer'])
-			li.addClass('active');
-		li.text(user['name']);
-		list.append(li);
-	}
-	players.append($('<div>').addClass('player-list').append(list));
+	if(game['status'] == 'playing'){
+		var left = $('<div>').addClass('game-item-left');
+		var players = $('<div>').addClass('players').append($('<div>').addClass('players-title').text('Players'));	
+		var list = $('<ul>');
+		for(var i=0; i<game.users.length; i++){
+			var user = game.users[i];
+			var li = $('<li>').addClass(user['color']);
+			if(user['id'] == game['currentPlayer'])
+				li.addClass('active');
+			li.text(user['name']);
+			list.append(li);
+		}
+		players.append($('<div>').addClass('player-list').append(list));
 
-	players.append($('<div>').addClass('last-move').text('Last Move:').append($('<span>').text(game['updated'].substr(0,7))))
+		players.append($('<div>').addClass('last-move').text('Last Move:').append($('<span>').text(game['updated'].substr(0,7))))
 
-	left.append(players);
+		left.append(players);
 
-	var right = $("<div>").addClass('game-item-right');
-	var status = $('<div>').addClass('status');
-	var button = $('<div>').addClass('action-button');
-	if(this.userid == game['currentPlayer']){
-		status.addClass('green');
-		status.text('Your Turn!');
-		button.text('Play');
+		var right = $("<div>").addClass('game-item-right');
+		var status = $('<div>').addClass('status');
+		var button = $('<div>').addClass('action-button');
+		if(this.userid == game['currentPlayer']){
+			status.addClass('green');
+			status.text('Your Turn!');
+			button.text('Play');
+		}
+		else{
+			status.addClass('red');
+			status.text('Waiting');
+			button.text('View')
+		}
+		right.append(status).append(button);
+		newGameContainer.append(left);
+		newGameContainer.append(right);
+
+		button.click(function(e){
+			e.stopPropagation();
+			window.location.pathname='/assimilation/play/' + game.id;
+		});
+
 	}
-	else{
-		status.addClass('red');
-		status.text('Waiting');
-		button.text('View')
+	else {//if(game['status'] == 'init'){
+		var message = $('<div>').addClass('message').text('Waiting for opponent to join.');
+		var deleteButton = $('<div>').addClass('delete').text('Delete Game');
+		deleteButton.click(function(e){
+			e.stopPropagation();
+			alert('Not Implemented');
+		});
+		newGameContainer.append(message).append(deleteButton);
 	}
-	right.append(status).append(button);
-	newGameContainer.append(left);
-	newGameContainer.append(right);
+	
 	this.games[game['id']] = newGameContainer;
 	this.container.append(newGameContainer);
-
-	button.click(function(e){
-		e.stopPropagation();
-		window.location.pathname='/assimilation/play/' + game.id;
-	});
-
 	newGameContainer.click(function(){
 		$('.game-list-item').removeClass('selected');
 		newGameContainer.addClass('selected');
